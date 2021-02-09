e3f3c20b867dfa902a5078a2d08f19be673ea814
Mikael Edlund <mikael.edlund@gmail.com>
2021-02-09 19:35:34 +0100

    jkhkj

--- micke.c
+++ micke.c
@@ -1,6 +1,6 @@
 
 #include "bi_ident.h"
-BI_IDENT("@(#)$Header: /tmp/git1/gittest/micke.c master 2021-02-09 19:19:26 micke Exp $");
+BI_IDENT("@(#)$Header: /tmp/git1/gittest/micke.c master 2021-02-09 19:35:30 micke Exp $");
 
 /*aasldsfdfdasasd  */
 
@@ -11,5 +11,6 @@ BI_IDENT("@(#)$Header: /tmp/git1/gittest/micke.c master 2021-02-09 19:19:26 mick
 int main(int argc, char *argv[])
 {
    printf("Micke\n");
+   printf("%s\n", RCSid);
 }
 

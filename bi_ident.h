2648a5b3b1cafc361620e30b51b575a1ce67514d
Mikael Edlund <mikael.edlund@gmail.com>
2021-02-09 19:41:45 +0100

    hkj

--- bi_ident.h
+++ bi_ident.h
@@ -1,10 +1,6 @@
 #ifndef BI_IDENT_H
 #define BI_IDENT_H
 
-#ifdef __GNUC__
 #define BI_IDENT(value) const char __attribute__((unused,weak)) * RCSid = value
-#else
-#define BI_IDENT(value) static char * RCSid = value
-#endif
 
 #endif

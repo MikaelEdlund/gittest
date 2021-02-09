9013d8ad96c39c4f75f2bfb4b373d6028461987c
Mikael Edlund <mikael.edlund@gmail.com>
2021-02-09 19:09:40 +0100

    jkhj

---
+++ micke.c
@@ -0,0 +1,15 @@
+
+#include "bi_ident.h"
+BI_IDENT("@(#)$Header: /home/nfs/jkjb/git2/jkjb_tools/test/micke.c master 2021-02-09 14:27:02 jkjb Exp $");
+
+/*aasdasdjkasldsfdfdasasd  */
+
+
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   printf("Micke\n");
+}
+
